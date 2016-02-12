1957582525c28213a529053ba0af2cd4c4ad1694
Alex Luecke <me@alexjl.com>
2016-02-12 14:09:57 -0800

    Wasn't replacing text in plugin with new plugin name.

--- rename.sh
+++ rename.sh
@@ -10,12 +10,12 @@ if [ -z "$PLUG_NAME" ] || [ -z "$PLUG_CLASS_NAME" ]; then
 	exit
 fi
 
-PLUG_FILES=$(find . -iname "$PLUG_DEFAULT_TEXT*")
+PLUG_FILES=$( find . -iname "$PLUG_DEFAULT_TEXT*" )
 
-echo -n "Modifying files:"
-for FILE in $PLUG_FILES; do echo $FILE; done;
+echo -e "Modifying files:"
+for FILE in $PLUG_FILES; do echo -e "\t$FILE"; done;
 
-echo "New plugin file prefix: $PLUG_NAME"
+echo -e "\nNew plugin file prefix: $PLUG_NAME"
 echo "New plugin PHP class prefix: $PLUG_CLASS_NAME"
 echo -e "\nIs this what you want?"
 select yn in "Yes" "No"; do
@@ -26,14 +26,14 @@ select yn in "Yes" "No"; do
 	esac
 done
 
-PLUG_FILES=$(find . -iname "$PLUG_DEFAULT_TEXT*")
-
 for FILE in $PLUG_FILES; do
 	NEW_FILE=$( echo "$FILE" | sed -e "s/$PLUG_DEFAULT_TEXT/$PLUG_NAME/g" )
 	echo "== Modifying file: $FILE =="
 	echo -e "\tReplacing text $PLUG_DEFAULT_CLASS with $PLUG_CLASS_NAME"
+	echo -e "\tReplacing text $PLUG_DEFAULT_TEXT with $PLUG_NAME"
 	echo -e "\tMoving $FILE to $NEW_FILE"
 	echo ""
 	sed -i "s/$PLUG_DEFAULT_CLASS/$PLUG_CLASS_NAME/g" $FILE
+	sed -i "s/$PLUG_DEFAULT_TEXT/$PLUG_NAME/g" $FILE
 	mv $FILE $NEW_FILE;
 done;
