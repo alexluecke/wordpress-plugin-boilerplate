d6ee881b70f84ea353112ff7b321279ce841aefe
Alex Luecke <me@alexjl.com>
2016-02-12 13:53:24 -0800

    Mode display text modifications.

--- rename.sh
+++ rename.sh
@@ -10,12 +10,17 @@ if [ -z "$PLUG_NAME" ] || [ -z "$PLUG_CLASS_NAME" ]; then
 	exit
 fi
 
+PLUG_FILES=$(find . -iname "$PLUG_DEFAULT_TEXT*")
+
+echo -n "Modifying files:"
+for FILE in $PLUG_FILES; do echo $FILE; done;
+
 echo "New plugin file prefix: $PLUG_NAME"
 echo "New plugin PHP class prefix: $PLUG_CLASS_NAME"
-echo "Is this what you want?\n"
+echo -e "\nIs this what you want?"
 select yn in "Yes" "No"; do
 	case $yn in
-		Yes ) echo "Continuing..."; break;;
+		Yes ) echo -e "\nContinuing...\n"; break;;
 		No ) echo "Exiting"; exit;;
 		* ) exit;;
 	esac
@@ -26,8 +31,8 @@ PLUG_FILES=$(find . -iname "$PLUG_DEFAULT_TEXT*")
 for FILE in $PLUG_FILES; do
 	NEW_FILE=$( echo "$FILE" | sed -e "s/$PLUG_DEFAULT_TEXT/$PLUG_NAME/g" )
 	echo "== Modifying file: $FILE =="
-	echo "\tReplacing text $PLUG_DEFAULT_CLASS with $PLUG_CLASS_NAME"
-	echo "\tMoving $FILE to $NEW_FILE"
+	echo -e "\tReplacing text $PLUG_DEFAULT_CLASS with $PLUG_CLASS_NAME"
+	echo -e "\tMoving $FILE to $NEW_FILE"
 	echo ""
 	sed -i "s/$PLUG_DEFAULT_CLASS/$PLUG_CLASS_NAME/g" $FILE
 	mv $FILE $NEW_FILE;
