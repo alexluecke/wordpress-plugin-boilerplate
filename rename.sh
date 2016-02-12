8acb3f221886f2e3841a68b3c4e4dd4050098f70
Alex Luecke <me@alexjl.com>
2016-02-12 13:47:32 -0800

    Display text.

--- rename.sh
+++ rename.sh
@@ -25,8 +25,9 @@ PLUG_FILES=$(find . -iname "$PLUG_DEFAULT_TEXT*")
 
 for FILE in $PLUG_FILES; do
 	NEW_FILE=$( echo "$FILE" | sed -e "s/$PLUG_DEFAULT_TEXT/$PLUG_NAME/g" )
-	echo "== Replacing text $PLUG_DEFAULT_CLASS with $PLUG_CLASS_NAME =="
-	echo "== Moving $FILE to $NEW_FILE =="
+	echo "== Modifying file: $FILE =="
+	echo "\tReplacing text $PLUG_DEFAULT_CLASS with $PLUG_CLASS_NAME"
+	echo "\tMoving $FILE to $NEW_FILE"
 	echo ""
 	sed -i "s/$PLUG_DEFAULT_CLASS/$PLUG_CLASS_NAME/g" $FILE
 	mv $FILE $NEW_FILE;
